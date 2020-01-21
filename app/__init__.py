349e466ee787f22e61aa50149328517e9be716b6
Veysel Şahin <thevyxx@gmail.com>
2020-01-21 12:54:07 +0300

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+
+from flask import Flask, Response
+import uuid
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return Response(str(uuid.uuid4()), status=200, 
+mimetype='text/plain')
+
